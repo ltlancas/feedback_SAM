443fd0315009b75654b431c52f01bddab26f7459
Lachlan Lancaster <lachlanlancaster@Lachlans-MacBook-Pro.local>
2024-08-02 12:13:36 -0400

    better variable naming

--- fb_models.py
+++ fb_models.py
@@ -322,7 +322,8 @@ class JointBubbleUncoupled(Bubble):
             print("Units of t are off")
             assert(False)
         ri = self.spitz_bubble.radius(t)*(t<self.teq)
-        solution =  self.joint_sol.sol(((t-self.teq)/self.tdio).to(" ").value)
+        chi = ((t-self.teq)/self.tdio).to(" ").value
+        solution =  self.joint_sol.sol(chi)
         ri += solution[0]*self.Rch*(t>self.teq)
         return ri.to("pc")
 
@@ -337,7 +338,8 @@ class JointBubbleUncoupled(Bubble):
         # up until teq the wind bubble follows the normal momentum-driven solution
         rw = self.wind_bubble.radius(t)*(t<self.teq)
         # afterwards it follows the joint evolution solution
-        solution =  self.joint_sol.sol(((t-self.teq)/self.tdio).to(" ").value)
+        chi = ((t-self.teq)/self.tdio).to(" ").value
+        solution =  self.joint_sol.sol(chi)
         xiw = self.get_xiw(solution[0])
         rw += xiw*self.Rch*(t>self.teq)
         return rw.to("pc")
@@ -352,7 +354,8 @@ class JointBubbleUncoupled(Bubble):
         
         # up until teq the ionized bubble follows the Spitzer solution
         vi = self.spitz_bubble.velocity(t)*(t<self.teq)
-        solution =  self.joint_sol.sol(((t-self.teq)/self.tdio).to(" ").value)
+        chi = ((t-self.teq)/self.tdio).to(" ").value
+        solution =  self.joint_sol.sol(chi)
         vi += solution[1]*self.Rch/self.tdio*(t>self.teq)
         return vi.to("km/s")
     
@@ -364,7 +367,8 @@ class JointBubbleUncoupled(Bubble):
             print("Units of t are off")
             assert(False)
         prefac = 4*np.pi*self.rho0*self.Rch**4/(3*self.tdio)
-        solution =  self.joint_sol.sol(((t-self.teq)/self.tdio).to(" ").value)
+        chi = ((t-self.teq)/self.tdio).to(" ").value
+        solution =  self.joint_sol.sol(chi)
         pr = prefac*solution[1]*solution[0]**3*(t>self.teq)
         pr += self.spitz_bubble.momentum(t)*(t<self.teq)
         pr += self.wind_bubble.momentum(t)*(t<self.teq)
